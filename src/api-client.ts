f22201850aa9e3b4fba6a0eeadd1bcc7b10e60f7
Huynh Duc Dung <dunghd.it@gmail.com>
2023-04-22 15:03:03 +0800

    feat: add runtime exaple with zod

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,7 +1,11 @@
-import { createTypeLevelClient, initUntypeable } from "untypeable";
+import {
+  createSafeClient,
+  createTypeLevelClient,
+  initUntypeable,
+} from "untypeable";
 import wretch from "wretch";
+import { z } from "zod";
 
-// TODO: add runtime type checking with zod
 // Initialize untypeable
 const u = initUntypeable();
 
@@ -22,15 +26,29 @@ const fetcherRouter = u.router({
 });
 
 const mutationRouter = u.router({
-  "/posts": u.input<Omit<Post, "id">>().output<Post>(),
+  "/posts": u
+    .input(
+      z.object({
+        title: z.string(),
+        body: z.string(),
+        userId: z.number(),
+      })
+    )
+    .output(
+      z.object({
+        id: z.number(),
+        title: z.string(),
+        body: z.string(),
+        userId: z.number(),
+      })
+    ),
 });
 
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 const externalApi = wretch(BASE_URL, { mode: "cors" }).errorType("json");
 
-// TODO: support mutations (PUT, PATCH, DELETE)
-// Possible solution is to define each method as a separate router
+// TODO: support mutations (PUT, PATCH, DELETE), refer to https://github.com/total-typescript/untypeable#pusharg
 export const fetcher = createTypeLevelClient<typeof fetcherRouter>(
   async (path, input) => {
     const res = externalApi.get(`${path}?${new URLSearchParams(input)}`);
@@ -38,9 +56,7 @@ export const fetcher = createTypeLevelClient<typeof fetcherRouter>(
   }
 );
 
-export const mutation = createTypeLevelClient<typeof mutationRouter>(
-  async (path, body) => {
-    const res = externalApi.url(path).post(body);
-    return res.json();
-  }
-);
+export const mutation = createSafeClient(mutationRouter, async (path, body) => {
+  const res = externalApi.url(path).post(body);
+  return res.json();
+});
